6e5dfaa122dad7eedde8b76ad076a5f9d6e51ab6
furugenict8 <furugenict8@gmail.com>
2020-06-10 17:38:46 +0900

    second commit

---
+++ lib/video_detail_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class VideoDetailPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Container(
+          child: Text('動画'),
+        ),
+      ),
+    );
+  }
+
+}
