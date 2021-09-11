e60a018614c89d6642c0306e0fd44323300eb689
Ryouhei Furugen <furugenict8@gmail.com>
2021-09-11 18:18:44 +0900

    primaryColorをwhiteに変更

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(App());
+  runApp(App()); //App classのインスタンスを引数に動く関数runApp()
 }
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(primaryColor: Colors.white),
       home: Scaffold(
         appBar: AppBar(
           title: const Text('超いけてるKBOYのFlutter大学'),
