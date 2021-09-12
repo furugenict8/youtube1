fd1c79009e22db5ed0efeeba101e43f70ab7c7df
Ryouhei Furugen <furugenict8@gmail.com>
2021-09-13 08:33:23 +0900

    Flutter大学アイコン　と　登録ボタンを ボディの上部に追加

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,9 @@ void main() {
 }
 
 class App extends StatelessWidget {
+  //appBarのaction buttonsのSizedBoxの幅　の変数sizeBoxWidthを作っておく。
+  double _sizeBoxWidth = 40;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,44 +17,82 @@ class App extends StatelessWidget {
           leading: Icon(Icons.more_vert),
           title: const Text('超いけてるKBOYのFlutter大学'),
           actions: [
-            IconButton(
-              icon: const Icon(Icons.search),
-              tooltip: '作成',
-              onPressed: () {
-                // handle the press
-              },
+            SizedBox(
+              width: _sizeBoxWidth,
+              child: IconButton(
+                icon: const Icon(Icons.search),
+                tooltip: '作成',
+                onPressed: () {
+                  // handle the press
+                },
+              ),
             ),
-            IconButton(
-              icon: const Icon(Icons.video_call_outlined),
-              tooltip: '作成',
-              onPressed: () {
-                // handle the press
-              },
+            SizedBox(
+              width: _sizeBoxWidth,
+              child: IconButton(
+                icon: const Icon(Icons.video_call_outlined),
+                tooltip: '作成',
+                onPressed: () {
+                  // handle the press
+                },
+              ),
             ),
-            IconButton(
-              icon: const Icon(Icons.apps),
-              tooltip: 'youtubeアプリ',
-              onPressed: () {
-                // handle the press
-              },
+            SizedBox(
+              width: _sizeBoxWidth,
+              child: IconButton(
+                icon: const Icon(Icons.apps),
+                tooltip: 'youtubeアプリ',
+                onPressed: () {
+                  // handle the press
+                },
+              ),
             ),
-            IconButton(
-              icon: const Icon(Icons.notifications_none),
-              tooltip: '通知',
-              onPressed: () {
-                // handle the press
-              },
+            SizedBox(
+              width: _sizeBoxWidth,
+              child: IconButton(
+                icon: const Icon(Icons.notifications_none),
+                tooltip: '通知',
+                onPressed: () {
+                  // handle the press
+                },
+              ),
             ),
-            IconButton(
-              icon: const Icon(Icons.account_circle),
-              tooltip: '通知',
-              onPressed: () {
-                // handle the press
-              },
+            SizedBox(
+              width: _sizeBoxWidth,
+              child: IconButton(
+                icon: const Icon(Icons.account_circle),
+                tooltip: '通知',
+                onPressed: () {
+                  // handle the press
+                },
+              ),
             )
           ],
         ),
-        body: Container(),
+        body: Container(
+          child: Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Image.network(
+                      "https://yt3.ggpht.com/Acyl-4aTTupNoyoxGGRVoC46a-cxqDk1_SC1JiuV-_XeHSeZBZqNJCjLlekP85t7Iw1qqp5aSA=s88-c-k-c0x00ffffff-no-rj"),
+                  Column(
+                    children: <Widget>[
+                      const Text("KboyのFlutter大学"),
+                      IconButton(
+                        onPressed: () {
+                          //登録画面へ繊維
+                        },
+                        icon:
+                            Icon(Icons.video_call_outlined, color: Colors.blue),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
